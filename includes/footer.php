5cd060da05a5208d040b11fce5c92822d229aaa8
alexander <6828@roc-teraa.nl>
2020-05-29 13:16:22 +0200

    header footer en home pagina gemaakt

---
+++ includes/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexander
+ * Date: 28/05/2020
+ * Time: 09:37
+ */
+?>
+<div class=".container" id="footer">
+    <div class="row" align-items-center>
+        <div class="col-sm" id="col1">
+            <a href="#">Home   &emsp; &ensp; &nbsp; </a>
+            <a href="#">Galerij<br></a>
+            <a href="#">Reserveren  &emsp;</a>
+            <a href="#">Contact</a>
+        </div>
+        <div class="col-sm" id="col2">
+            <p>Copyright Rent My Cabin</p>
+        </div>
+        <div class="col-sm" id="col3">
+            <a href="#">facebook<br></a>
+            <a href="#">twitter<br></a>
+            <a href="#">instagram<br></a>
+        </div>
+    </div>
+</div>
+
+
---
+++ includes/header.php
@@ -0,0 +1,38 @@
+<div class=".container-fluid" id="header">
+    <div class="row">
+        <div class="col" >
+            <h1>Rent My Cabin</h1>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col" >
+            <nav class="navbar navbar-expand-lg navbar-light ">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="reserveren.php">Reserveren </a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="galerij.php">Galerij</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="contact.php">Contact</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="login.php" >Login</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+
+        </div>
+    </div>
+</div>
